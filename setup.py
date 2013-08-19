58d953e810fd9030ab0d6e1ad5241c6680a1ac2a
Matt Roberts <matt@mediaburst.co.uk>
2013-08-19 16:39:27 +0100

    New files for the project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ClockworkPython',
+    version='1.0',
+    packages=['clockwork-python'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
