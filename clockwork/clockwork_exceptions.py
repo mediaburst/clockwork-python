ad16c523341f71f19c9334ab91f29555caa51eb9
Spencer McIntyre <zeroSteiner@gmail.com>
2016-02-06 22:30:39 -0500

    Remove the lxml dependency

--- clockwork/clockwork_exceptions.py
+++ clockwork/clockwork_exceptions.py
@@ -3,18 +3,21 @@
 class HttpException(Exception):
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return repr(self.value)
 
 class AuthException(Exception):
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return repr(self.value)
 
 class GenericException(Exception):
     def __init__(self, value):
         self.value = value
+
     def __str__(self):
         return repr(self.value)
 
@@ -22,5 +25,6 @@ class ApiException(Exception):
     def __init__(self, value, errNum):
         self.value = value
         self.errNum = errNum
+
     def __str__(self):
         return repr(self.value)
